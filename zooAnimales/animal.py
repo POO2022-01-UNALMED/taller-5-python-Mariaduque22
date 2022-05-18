882aade582a72f8f71624fcc485cfcdbe5adcd92
Maria Paula Duque <92931247+Mariaduque22@users.noreply.github.com>
2022-05-17 21:51:37 -0500

    ss

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,44 +1,41 @@
 class Animal:
-    _totalAnimales=0
+    _totalAnimales = 0
 
-    
-    def __init__(self,nombre,edad,habitat,genero,zona=None):
+    def __init__(self, nombre, edad, habitat, genero):
         self._nombre = nombre
         self._edad = edad
         self._habitat = habitat
         self._genero = genero
-        self._zona = zona
-        Animal._totalAnimales+=1
+        self._zona = None
+        self._totalAnimales += 1
 
-    def movimiento():
-        return "desplazarse"   
-    
+    def getNombre(self):
+        return self._nombre
+
+    def getEdad(self):
+        return self._edad
+
+    def getHabitat(self):
+        return self._habitat
+
+    def getGenero(self):
+        return self._genero
+
+    def movimiento(self):
+        return "moving"
+
+    @staticmethod
+    def totalPorTipo():
+        from zooAnimales.mamifero import Mamifero
+        from zooAnimales.ave import Ave
+        from zooAnimales.reptil import Reptil
+        from zooAnimales.anfibio import Anfibio
+        from zooAnimales.pez import Pez
+        return f"Mamiferos : {Mamifero.cantidadMamiferos()}\nAves : {Ave.cantidadAves()}\nReptiles : {Reptil.cantidadReptiles()}\nPeces : {Pez.cantidadPeces()}\nAnfibios : {Anfibio.cantidadAnfibios()}"
+
     def toString(self):
-        if self._zona is not None: 
-            return "Mi nombre es "+self.getNombre()+", tengo una edad de "+str(self.getEdad())+", habito en "+self.getHabitat()+" y mi genero es "+self.getGenero()+", la zona en la que me ubico es "+self.getZona()+", en el zoo "+"PENDIENTE"
+        message = f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}"
+        if self._zona:
+            return f"{message}, la zona en la que me ubico es {self._zona.getNombre()}, en el {self._zona.getZoo().getNombre()}"
         else:
-            return "Mi nombre es "+self.getNombre()+", tengo una edad de "+str(self.getEdad())+", habito en "+self.getHabitat()+" y mi genero es "+self.getGenero()
-        
-        
-    
-    @classmethod
-    def getTotalAnimales(cls):
-        return cls._totalAnimales
-    @classmethod
-    def totalPorTipo(cls):
-        hijos = cls.__subclasses__()
-        return "Mamiferos : "+str(hijos[0].cantidadMamiferos())+"\nAves : "+str(hijos[1].cantidadAves())+"\nReptiles : "+str(hijos[2].cantidadReptiles())+"\nPeces : "+str(hijos[3].cantidadPeces())+"\nAnfibios : "+str(hijos[4].cantidadAnfibios())
-        
-    @classmethod
-    def cantidadAnfibios(cls): return cls._totalAnfibios
-    
-    def setNombre(self,a): self._nombre=a
-    def getNombre(self): return self._nombre
-    def setEdad(self,a): self._edad=a
-    def getEdad(self): return self._edad
-    def setHabitat(self,a): self._habitat=a
-    def getHabitat(self): return self._habitat
-    def setGenero(self,a): self._genero=a
-    def getGenero(self): return self._genero
-    def setZona(self,a): self._zona=a
-    def getZona(self): return self._zona
+            return message
